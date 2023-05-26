8587231577342b2d07bf986ecec0983226173eef
Olalla Díaz-Yáñez <olalla.diaz@usys.ethz.ch>
2023-05-26 07:31:32 +0200

    add NA as default value for user parameter file creation

--- R/configLandClim.R
+++ R/configLandClim.R
@@ -2,14 +2,13 @@
 #'
 #' @param overwrite default is FALSE, indicating that if the xml files already exist they will not been overwritten. A warning appears as a function return.
 #' @param model the path to the model configuration file as a character or a data.frame where each of the variables/columns is a tag in the model configuration file in LandClim. If not provided the default values will be used.
-#' @param barkbeetle the path to the barkbeetle configuration file as a character or a data.frame where each of the variables/columns is a tag in the bark beetle configuration file in LandClim. If not provided the default values will be used. The created file will be saved in the indicated workspacepath.
-#' @param landtype  the path to the landtype configuration file as a character or a data.frame where each of the variables/columns is a tag in the land type parameters configuration file in LandClim. If not provided the default values will be used. The created file will be saved in the indicated workspacepath.
-#' @param planting  the path to the planting configuration file as a character or a data.frame where each of the variables/columns is a tag in the planting parameters configuration file in LandClim. If not provided the default values will be used. The created file will be saved in the indicated workspacepath.
-#' @param randomstate the path to the randomstate configuration file  as a character or a data.frame where each of the variables/columns is a tag in the random state configuration file in LandClim. If not provided the default values will be used. The created file will be saved in the indicated workspacepath.
-#' @param species the path to the species configuration file  as a character or a data.frame where each of the variables/columns is a tag in the species parameters configuration file in LandClim. If not provided the default values will be used. The created file will be saved in the indicated workspacepath.
+#' @param barkbeetle the path to the barkbeetle configuration file as a character or a data.frame where each of the variables/columns is a tag in the bark beetle configuration file in LandClim. If not provided the default values will be used. The created file will be saved in the indicated workspacepath. If NULL is assigned the parameter file wont be created.
+#' @param landtype  the path to the landtype configuration file as a character or a data.frame where each of the variables/columns is a tag in the land type parameters configuration file in LandClim. If not provided the default values will be used. The created file will be saved in the indicated workspacepath.If NULL is assigned the parameter file wont be created.
+#' @param planting  the path to the planting configuration file as a character or a data.frame where each of the variables/columns is a tag in the planting parameters configuration file in LandClim. If not provided the default values will be used. The created file will be saved in the indicated workspacepath.If NULL is assigned the parameter file wont be created.
+#' @param randomstate the path to the randomstate configuration file  as a character or a data.frame where each of the variables/columns is a tag in the random state configuration file in LandClim. If not provided the default values will be used. The created file will be saved in the indicated workspacepath.If NULL is assigned the parameter file wont be created.
+#' @param species the path to the species configuration file  as a character or a data.frame where each of the variables/columns is a tag in the species parameters configuration file in LandClim. If not provided the default values will be used. The created file will be saved in the indicated workspacepath.If NULL is assigned the parameter file wont be created.
 #' @param workspacePath path where the the input xml configuration files  and output will be created. The name of the files will be the default names (config.xml, barkbeetle.xml, landtype.xml, planting.xml, randomstate.xml, species.xml) unless they have previously created.
-#' @param binPath path to the folder where LandClim model binary files needed for your operative system are storage.
-#' @return Run LandClim with the desired input files and saved in the \code{workspacePath} provided.
+#' @return The desired input parameter files and saved them in the \code{workspacePath} provided.
 #' @examples
 #' runLandClim(overwrite = FALSE,
 #'             model = NA,
@@ -19,18 +18,17 @@
 #'             randomstate =  NA,
 #'             species = NA,
 #'             workspacePath = "workspace",
-#'             consoleOutputPath = "",
-#'             binPath = "bin")
+#'             consoleOutputPath = "")
 
 
 configLandClim <- function(overwrite = FALSE,
                            decadal,
-                           model = NULL,
-                           barkbeetle = NULL,
-                           landtype = NULL,
-                           planting = NULL,
-                           randomstate =  NULL,
-                           species = NULL,
+                           model = NA,
+                           barkbeetle = NA,
+                           landtype = NA,
+                           planting = NA,
+                           randomstate =  NA,
+                           species = NA,
                            workspacePath = "workspace/") {
 
   # Load default values
@@ -48,14 +46,21 @@ configLandClim <- function(overwrite = FALSE,
     dir.create(workspacePath, recursive = TRUE)
   }
 
+  #OPTION 0 - user parameter file is not created
+  if (is.null(model)) {print("Model user parameter file is not created")}
+  if (is.null(barkbeetle)) {print("Barkbeetle user parameter file is not created")}
+  if (is.null(landtype)) {print("Landtype user parameter file is not created")}
+  if (is.null(planting)) {print("Planting user parameter file is not created")}
+  if (is.null(randomstate)) {print("Randomstate user parameter file is not created")}
+  if (is.null(species)) {print("Species user parameter file is not created")}
 
   # OPTION A- No parameters are provided, use the default values and create the xml file in workspacePath
-  if (is.null(model)) {model <- default_model}
-  if (is.null(barkbeetle)) {barkbeetle <- default_barkbeetle}
-  if (is.null(landtype)) {landtype <- default_landtype}
-  if (is.null(planting)) {planting <- default_planting}
-  if (is.null(randomstate)) {randomstate <- default_randomstate}
-  if (is.null(species)) {species <- default_species}
+  if (is.na("default")) {model <- default_model}
+  if (is.na("default")) {barkbeetle <- default_barkbeetle}
+  if (is.na("default")) {landtype <- default_landtype}
+  if (is.na("default")) {planting <- default_planting}
+  if (is.na("default")) {randomstate <- default_randomstate}
+  if (is.na("default")) {species <- default_species}
 
   #OPTION B - provide your own parameter values, create the xml file in workspacePath
   # or has been assigned to the default values in the step above
