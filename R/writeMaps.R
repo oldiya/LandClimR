92e5f3be0cc332a6e69719fdc83f94742c361a5b
Olalla Díaz-Yáñez <olalla.diaz@usys.ethz.ch>
2023-05-27 10:24:01 +0200

    add resolution check and change of resulution if needed (both via agregation of disagregation) in the function writeMaps

--- R/writeMaps.R
+++ R/writeMaps.R
@@ -17,9 +17,25 @@ writeMaps <- function(landClimRasterStack,
                                      function(x) raster::crop(x, ex))
   rs <- raster::stack(landClimRasterStack_list)
   names(rs) <- names(landClimRasterStack)
+
+  if (!unique(raster::res(rs) == lcResolution)){
+    if (raster::res(rs)[1] > lcResolution){
+      rres <- raster::res(rs) / lcResolution
+      if (unique(!rres%%1==0)){ print("the lcresolution is not a fraction of the current map resolution, please change lcresolution")}
+      #disaggregate from res(rs) resolution to lcResolution (factor = rres)
+      rs_disag <- lapply(landClimRasterStack_list, raster::disaggregate, fact=rres)
+      landClimRasterStack_list <- rs_disag
+    } else if (raster::res(rs)[1] < lcResolution){
+      rres <-  lcResolution / raster::res(rs)[1]
+      if (unique(!rres%%1==0)){print("the lcresolution is not a fraction of the current map resolution, please change lcresolution")}
+      #aggregate from raster::res(rs) resolution to lcResolution(factor = rres)
+      rs_ag <- lapply(landClimRasterStack_list, raster::aggregate, fact=rres)
+      landClimRasterStack_list <- rs_ag
+    }
+  }
   #raster::writeRaster(rs, filename=paste0(folder, "/landclim_maps.tif"), overwrite = TRUE)
   rm(rs)
-  foo <- function(x){
+  foo <- function(x) {
     sink(paste(folder, "/", names(x), ".asc", sep = ""))
     writeLines(c(paste("ncols", ncol(x)),
                  paste("nrows", nrow(x)),
@@ -32,6 +48,6 @@ writeMaps <- function(landClimRasterStack,
                 file = paste(folder, "/", names(x), ".asc", sep = ""), append = TRUE,
                 quote = FALSE, row.names = FALSE, col.names = FALSE)
   }
-  lapply(landClimRasterStack_list, function(x) foo(x))
+  invisible(lapply(landClimRasterStack_list, function(x) foo(x)))
 }
 
